f3d501ba58177ebcefa97816884f19da20106417
ViridianMelody <mlove@anaconda.com>
2025-11-13 15:48:25 -0800

    Adjust variables for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 
-.\configure --prefix=$PREFIX --libdir=$PREFIX\lib --disable-static --enable-shared
-make -j${CPU_COUNT}
+.\configure --prefix=%PREFIX% --libdir=%PREFIX%\lib --disable-static --enable-shared
+make -j%CPU_COUNT%
 make install
 
 if errorlevel 1 exit 1
